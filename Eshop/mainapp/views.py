c545b5242b1e1ccdb93e665d68e34a82cdbc1445
ananthakrishnanpta <ananthakrishnanpta@gmail.com>
2026-01-05 12:28:33 +0530

    added pages

--- Eshop/mainapp/views.py
+++ Eshop/mainapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def homeView(request):
+    template = 'mainapp/home.html'
+    context  = {}
+
+    return render(request, template_name= template, context= context)
+
+def aboutView(request):
+    template = 'mainapp/about.html'
+    context = {}
+    return render(request, template, context)
