209985fb8370c58a857ad22257ea4cce10424487
ananthakrishnanpta <ananthakrishnanpta@gmail.com>
2026-01-22 12:06:13 +0530

    edits

--- Eshop/products/views.py
+++ Eshop/products/views.py
@@ -48,6 +48,12 @@ class ProductDetail(DetailView):
     template_name = 'products/product_details.html'
     context_object_name = 'product'
 
+    # overriding the queryset to pre-fetch 
+    # and add the product images alongside products
+    def get_queryset(self):
+        return Product.objects.prefetch_related('images')
+
+
 class UpdateProduct(UpdateView):
     model = Product
     fields = '__all__'
